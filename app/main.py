a71a96fd318ba72b01340c99cd5240f63837b88f
xdevore <54865093+xdevore@users.noreply.github.com>
2020-10-06 21:54:20 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,14 @@ app = FastAPI()
 
 time = { "Time in PST: " : datetime.now() }
 
-@app.get("/")
+@app.get("/get time/")
 def current_time():
     return time
+
+@app.get("/")
+def get_current_time():
+    final = time.get("Time in PST: ")
+    return final
+
+def pacific_time():
+    return pytz.timezone("US/Pacific")
