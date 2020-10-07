bbb5bcd0205c4916df058c80683c98165e862fc2
xdevore <54865093+xdevore@users.noreply.github.com>
2020-10-06 21:07:20 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def get_time():
-    return { "Time in PST: ": datetime.now() }
+@app.get("/PST/")
+def pacific_time():
+    current_time = datetime.now()
+    pacific_time = current_time.astimezone(timezone('US/Pacific'))
+    return { "Time in PST: " : pacific_time }
